0b16b2367d15e640ad38bfa9599d8470ffd4dd96
Zao-q <80674641+Zao-q@users.noreply.github.com>
2023-04-17 13:04:16 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: false },
     { id: 1, name: locale.NAV.ABOUT, to: '/x', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: false },
+    { id: 2, name: locale.NAV.RSS, to: '/feed', show: false, external: true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: false }
   ]
   return (
@@ -24,7 +24,8 @@ const NavBar = () => {
                 key={link.id}
                 className="block ml-4 text-black dark:text-gray-50 nav"
               >
-                <Link href={link.to}>{link.name}</Link>
+                <Link href={link.to} target={link.external ? '_blank' : null}>
+{link.name}</Link>
               </li>
             )
         )}
