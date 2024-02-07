d420217f5ada806642035e1f40fb307136f9942f
Zao-q <80674641+Zao-q@users.noreply.github.com>
2024-02-07 13:13:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -6,12 +6,12 @@ const BLOG = {
   description: '',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'light', // ['light', 'dark', 'auto'],
   font: 'serif', // ['sans-serif', 'serif']
   lightBackground: '#FAF9F7', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#686f67', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 1888, // If leave this empty, current year will be used.
+  since: 2012, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: true,
   showAbout: false,
