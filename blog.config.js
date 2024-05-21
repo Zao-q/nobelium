2d448e8d52db8ba74e573414d662929ec3503aa6
Zao-q <80674641+Zao-q@users.noreply.github.com>
2024-05-21 10:13:45 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -6,7 +6,7 @@ const BLOG = {
   description: '',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'light', // ['light', 'dark', 'auto'],
   font: 'serif', // ['sans-serif', 'serif']
   lightBackground: '#FAF9F7', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#686f67', // use hex value, don't forget '#'
