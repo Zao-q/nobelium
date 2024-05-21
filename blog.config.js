de682bb26152e20e38132719de4ef6acee388a95
Zao-q <80674641+Zao-q@users.noreply.github.com>
2024-05-21 16:27:28 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '5cc7169d-6535-4e60-be5b-76ae81ff5d5b', // data-app-id
+      appId: 'bf72cd07-71b2-4cf5-b228-c18807d5f2ea', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
